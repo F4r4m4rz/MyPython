823bf69288534c9d6b7bee6810360dc5328bdb34
Faramarz Bodaghi <faramarz.bodaghi@outlook.com>
2020-08-06 22:40:18 +0200

    working with classes __call__ Class decorators

---
+++ WebScraping/scraping.py
@@ -0,0 +1,16 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrap():
+    url = "https://www.zalando.no/salg/?sale=true"
+    doc = requests.get(url)
+    soup = BeautifulSoup(doc.text, 'html.parser')
+    all = soup.select('div')
+    for a in all:
+        print(a)
+
+
+if __name__ == '__main__':
+    scrap()
